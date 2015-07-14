90522ff728b76aef952aeacf1ad2ac7f13401c9a
rovaira <rovaira@gmail.com>
2015-07-14 14:52:34 -0400

    user unit tests and topbar color

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :username, length: { in: 3..15 }
+  validates :username, uniqueness: true
+  validates :username, format: { without: /[^a-z0-9]/ }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it { should have_valid(:username).when('username') }
+  it { should have_valid(:username).when('anothername123') }
+  it { should_not have_valid(:username).when('') }
+  it { should_not have_valid(:username).when(nil) }
+  it { should_not have_valid(:username).when('with space') }
+  it { should_not have_valid(:username).when('upsernamethatgoesonforever') }
+  it { should_not have_valid(:username).when('with#$%^characters') }
+
+  it { should have_valid(:email).when('user@aexample.com', 'another@gmail.com') }
+  it { should_not have_valid(:email).when(nil, '', 'user', 'users@com', 'users.com', 'usersuser@anothername123') }
+
+  it 'has a matching password confirmation for the password' do
+    user = User.new
+    user.password = 'password'
+    user.password_confirmation = 'anotherpassword'
+
+    expect(user).to_not be_valid
+    expect(user.errors[:password_confirmation]).to_not be_blank
+  end
 end
